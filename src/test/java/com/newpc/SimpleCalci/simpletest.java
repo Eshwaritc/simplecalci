278a5ea8bc05f64e9430776a9fffc87751a876cb
Eshwaritc <eshwariuma2004@gmail.com>
2025-04-22 10:29:22 +0530

    Initial Maven project push

---
+++ src/test/java/com/newpc/SimpleCalci/simpletest.java
@@ -0,0 +1,14 @@
+package com.newpc.SimpleCalci;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class simpletest {
+
+	@Test
+	public void test() {
+		assertEquals(4, 2+2);
+	}
+
+}
